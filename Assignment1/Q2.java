ae8f8e5aa41825e0fe61f9c88efddf49f0f36414
ananyab5 <70006727+ananyab5@users.noreply.github.com>
2020-08-21 22:08:16 +0530

    Create Q2.java

---
+++ Assignment1/Q2.java
@@ -0,0 +1,52 @@
+package A2;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = scanner.nextLine();
+
+        System.out.print("Enter number of dict words: ");
+        int n2 = scanner.nextInt();
+
+        HashMap<String, Integer> dict= new HashMap<>();
+
+        for(int i=0; i<=n2; i++){
+            String temp = scanner.nextLine();
+            dict.put(temp, 1);
+        }
+
+        String[] textArray = text.split(" ");
+
+        StringBuffer result = new StringBuffer();
+
+        for(String str : textArray){
+
+            if(dict.containsKey(str)){
+
+                String s1 = String.valueOf(str.charAt(0));
+
+                StringBuffer s2 = new StringBuffer();
+
+                for(int i=0; i<str.length()-1; i++){
+                    s2.append('*');
+                }
+
+                result.append(s1+s2);
+            }
+            else{
+                result.append(str);
+            }
+
+            result.append(" ");
+        }
+
+        System.out.println(result);
+
+    }
+}
