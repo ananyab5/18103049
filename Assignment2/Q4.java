4f8acb75ca00068a6c38563092bc933c8e4ce2db
ananyab5 <70006727+ananyab5@users.noreply.github.com>
2020-09-04 22:03:49 +0530

    Create Q4.java

---
+++ Assignment2/Q4.java
@@ -0,0 +1,33 @@
+package A4;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        long n = scanner.nextInt();
+        long res = compute(n);
+
+        if(res == -1) System.out.println("Does not exist!");
+        else System.out.println(res);
+    }
+
+    private static long compute(long n){
+
+        long sum = 0;
+
+        for(long i=1; i<=n; i++){
+
+            sum += i;
+
+            if(sum == i*i && sum > Integer.MAX_VALUE)
+                return i;
+        }
+
+        return -1;
+    }
+}
