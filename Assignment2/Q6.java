f7bd763a8965af206743dd23b9b2d57f29f7b12d
ananyab5 <70006727+ananyab5@users.noreply.github.com>
2020-09-04 22:04:36 +0530

    Create Q6.java

---
+++ Assignment2/Q6.java
@@ -0,0 +1,40 @@
+package A6;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a positive number: ");
+
+        long n; // input
+
+        while(true){
+
+            n = scanner.nextLong();
+
+            if(n <= 0) System.out.println("Invalid Negative Number");
+
+            else break;
+        }
+
+        hailStoneSeq(n);
+    }
+
+    private static void hailStoneSeq(long n){
+
+        while(n != 1 && n <= Integer.MAX_VALUE){
+
+            System.out.println(n);
+
+            if(n%2 == 0) n /= 2;
+            else n = n*3 + 1;
+        }
+
+        if(n == 1) System.out.println(n);
+        else System.out.println("Integer Overflow\nEnter A Smaller Input");
+    }
+}
